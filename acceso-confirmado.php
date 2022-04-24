cefe7bf28ae787ec72cf71a7cbe7958f7a831d4c
Henmar13 <you@example.com>
2022-04-24 17:07:58 -0300

    cambios

---
+++ acceso-confirmado.php
@@ -0,0 +1,42 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceso confirmado</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 py-3">
+                <h1>Bienvenid@ al Sistema</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="alert alert-primary col-12" role="alert">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi reiciendis quaerat dolor et commodi excepturi nulla, quos iure reprehenderit debitis ex necessitatibus! Praesentium error placeat vero sed modi saepe porro.</p>                
+            </div>
+            <div>
+                <a class="btn btn-primary float-end" href="index.php" role="button">Salir</a>
+            </div>
+        </div>
+     </main>
+    
+</body>
+</html>
+     
+
+
+    </main>
+    
+</body>
+</html>
